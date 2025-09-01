3e34cac74a3e206918ad028920b4dd12b5d2578f
Sarath Kumar Dunga <165943559+Sdunga1@users.noreply.github.com>
2025-09-01 00:38:25 -0700

    Delete polarion directory

--- polarion/pl_starter.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-sudo ./kill_postgres.sh
-sudo -u postgres /usr/lib/postgresql/17/bin/pg_ctl -D /opt/polarion/data/postgres-data -l /opt/polarion/data/postgres-data/log.out -o "-p 5433" start
-service apache2 start
-service polarion start
-
-wait
-tail -f /dev/null
